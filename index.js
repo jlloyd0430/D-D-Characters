1864c40389bb08068f3d830cb3ba1adf147f90eb
Jesse Lloyd <116366380+jlloyd0430@users.noreply.github.com>
2023-01-14 02:45:31 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ const app = express();
 const path = require("path");
 const logger = require("morgan");
 const methodOverride = require("method-override");
-
 // connection to our database
 const connectToMongoDB = require("./database/mongodb");
 
 /*
+    8. Set up necessary modules for login sessions
+*/
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const sessions = require("express-session");
+/*
     Setting up middleware
 */
-
-// We can use HTML methods with back-end methods smoothly
-app.use(methodOverride("_method"));
 // view engine settings
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -22,23 +24,51 @@ app.use(logger("dev"));
 // Read incoming requests properly
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// We can use HTML methods with back-end methods smoothly
+app.use(methodOverride("_method"));
 
+/*
+    9. Set up cookie parser middleware
+*/
+app.use(cookieParser(process.env.COOKIE_SECRET));
+/*
+    10. Set up the login session
+*/
+//24 hrs in milliseconds
+const oneDay = 1000 * 60 * 60 * 24;
+
+app.use(
+  sessions({
+    secret: process.env.COOKIE_SECRET,
+    saveUninitialized: false,
+    resave: false,
+    cookie: { maxAge: oneDay },
+  })
+);
 /*
     Connecting routers, using URL extensions
 */
-
 // Back-end
 const characterRouter = require("./routes/api/characterRouter");
-app.use("/api", characterRouter);
+app.use("/api/character", characterRouter);
 
-// front end
+/*
+    4. Plug in the user router
+*/
+const userRouter = require("./routes/api/userRouter");
+app.use("/api/user", userRouter);
+
+// Front-end
 const viewsRouter = require("./routes/viewRouters/viewRouter");
 app.use("/", viewsRouter);
 
-let PORT = 3333;
+/*
+    Turning the server on
+*/
+const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
+  console.log(`server is on ${PORT}`);
 
   connectToMongoDB();
 });
