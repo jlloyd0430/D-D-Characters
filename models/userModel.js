5cafd9bff78bd913ff9ba55d366d1b8423987651
Jesse Lloyd <116366380+jlloyd0430@users.noreply.github.com>
2023-01-14 02:42:53 -0500

    Add files via upload

---
+++ models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  favoriteCharacter: {
+    type: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Character",
+        // unique: true,
+      },
+    ],
+    default: [],
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
